95c604b92a6469e9480e9eb3e48d54d3b76dc66e
Sri Harsha Hari <91277025+sriharshahari1@users.noreply.github.com>
2024-07-11 14:07:14 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        DMSerifDisplay: ["DM Serif Display"]
+      }
+    },
+  },
+  plugins: [],
+});
